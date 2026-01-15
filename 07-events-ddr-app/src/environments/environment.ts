2c71a1ca43bcb02dd3d688e85dfde32ae3058e3a
Henar Mata San Juan <henar.mata@hpe.com>
2026-01-15 11:37:56 +0100

    app 7 finished

--- 07-events-ddr-app/src/environments/environment.ts
+++ 07-events-ddr-app/src/environments/environment.ts
@@ -12,8 +12,8 @@ export const environment = {
     messagingSenderId: "23605496158",
     appId: "1:23605496158:web:565e77d6926649b3b467cf"
   },
-  oneSignalAppId: "0c945589-2cc9-4ec2-b883-4256f1fdc116",
-  oneSignalRestApiKey: "os_v2_app_bskflcjmzfhmfoedijlpd7obcyazrzbip7duoq4gxzpywdg4exth5lmxgrrsodf3xrzao7ugmgmq7ypgp63bbyvbajhc3aohfb2pgyi"
+  oneSignalAppId: "",
+  oneSignalRestApiKey: ""
 };
 
 /*
